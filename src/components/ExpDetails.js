f3d31e195351cc8a70230be52734f3053677799f
Yuehan Duan <yuehanduan@yahoo.com>
2018-05-22 12:21:44 -0700

    Fix expdetails numvalue.

--- src/components/ExpDetails.js
+++ src/components/ExpDetails.js
@@ -25,7 +25,7 @@ const ExpDetails = props => {
               Essentials by OFM ESS-3085 Racing Style Leather Gaming Chair, Red
             </span>
             <div className="exp-details-footer">
-              <span id="exp-details-footer-num">${props.subtotal}.00</span>
+              <span id="exp-details-footer-num">${props.subtotal}</span>
               <span id="exp-details-qty">Qty: 1</span>
             </div>
             <div className="exp-details-color">
